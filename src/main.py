5ffb62a009a03bf788f15f8b79a0aca187e13f65
mn-n-o-m-a-d <mn07@gmx.at>
2026-03-13 19:38:11 +0100

    feat: add more cities

--- src/main.py
+++ src/main.py
@@ -19,16 +19,13 @@ def update_weather(lat, lon, name):
     except Exception as e:
         print(f"Error: {e}")
 
-# coordinates:
-    lat, lon = 48.25, 16.36 #Wien
-    lat, lon = 47.07, 15.43 #Graz
-    lat, lon = 47.50, 9.74 #Bregenz
-    lat, lon = 47.27, 11.40 #Innsbruck
-    lat, lon = 47.80, 13.03 #Salzburg
-    lat, lon = 48.31, 14.29 #Linz
-    lat, lon = 48.20, 15.62 #St. Pölten
-    lat, lon = 47.85, 16.52 #Eisenstadt
-    lat, lon = 46.62, 14.31 #Klagenfurt
-
 if __name__ == "__main__":
     update_weather(48.25, 16.36, "Wien")
+    update_weather(47.07, 15.43, "Graz")
+    update_weather(47.50, 9.74, "Bregenz")
+    update_weather(47.27, 11.40, "Innsbruck")
+    update_weather(47.80, 13.03, "Salzburg")
+    update_weather(48.31, 14.29, "Linz")
+    update_weather(48.20, 15.62, "StPölten")
+    update_weather(47.85, 16.52, "Eisenstadt")
+    update_weather(46.62, 14.31, "Klagenfurt")
