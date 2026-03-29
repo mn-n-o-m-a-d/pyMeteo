e18b0f13a7b9567baa70049e3dfc99cbc4794357
manuel <mn07@gmx.at>
2026-03-29 17:21:59 +0200

    feat: update stylecheck

--- src/main.py
+++ src/main.py
@@ -21,9 +21,10 @@ def get_city_weather(lat, lon, name, timestamp):
         print(f"Error at {name}: {e}")
         return None
 
+
 def get_forecast(lat, lon, name, timestamp):
     url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin"
-    
+
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -33,21 +34,20 @@ def get_forecast(lat, lon, name, timestamp):
         forecast_days = []
 
         for i in range(len(daily_data["time"])):
-            forecast_days.append({
-                "date": daily_data["time"][i],
-                "code": daily_data["weather_code"][i],
-                "temp_max": daily_data["temperature_2m_max"][i],
-                "temp_min": daily_data["temperature_2m_min"][i]
-            })
+            forecast_days.append(
+                {
+                    "date": daily_data["time"][i],
+                    "code": daily_data["weather_code"][i],
+                    "temp_max": daily_data["temperature_2m_max"][i],
+                    "temp_min": daily_data["temperature_2m_min"][i],
+                }
+            )
 
-        return {
-            "name": name,
-            "timestamp": timestamp,
-            "forecast": forecast_days
-        }
+        return {"name": name, "timestamp": timestamp, "forecast": forecast_days}
     except Exception as e:
         print(f"Error at {name}: {e}")
         return None
+
 
 if __name__ == "__main__":
     cities = [
@@ -71,7 +71,9 @@ if __name__ == "__main__":
             all_weather.append(weather)
         time.sleep(0.1)
 
-    with open("/home/manuel/VSCode/2026/pyMeteo/frontend/austria_weather.json", "w") as f:
+    with open(
+        "/home/manuel/VSCode/2026/pyMeteo/frontend/austria_weather.json", "w"
+    ) as f:
         json.dump(all_weather, f)
 
     print(f"{len(all_weather)} Cities updated.")
